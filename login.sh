0a5a7890a48c2c565d157d85b7067eb8c5f60e69
Raizen-01 <raizen-01@pop-os.localdomain>
2023-04-14 00:14:30 +0530

    Changed login.sh to automatically add the required files to autostart

--- login.sh
+++ login.sh
@@ -1,1 +1,14 @@
-python3 login.py
+
+cd ~/.config/autostart/
+
+touch my-selenium-script.desktop
+
+echo "[Desktop Entry]
+Type=Application
+Name=My Selenium Script
+Exec=python3 path/to/login.py
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true" > my-selenium-script.desktop
+
+nano my-selenium-script.desktop
